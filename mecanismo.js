d6004495cedb3b0335ddda044363624e67d521ed
cbssoares <cbssdev@gmail.com>
2023-04-29 16:43:33 -0300

    atualizações com flex-box

--- mecanismo.js
+++ mecanismo.js
@@ -1,5 +1,5 @@
 contador1 = 0
-document.querySelector('p#mainfooter').innerHTML = `${contador1} Participantes <input type="button" value="Conluido" id="proxeta" class="bota" onclick="mudarmain()">`
+document.querySelector('p#mainfooter').innerHTML = `${contador1} Participantes`
 nome = []
 document.querySelector('input#nomeinp').addEventListener("keyup", function(e) {
     if (e.code === 'Enter'){
@@ -16,14 +16,14 @@ function namesave(){
         lista.innerHTML += `<li>${nome[contador1]}`
         contador1++
         nominp.value = ""
-        document.querySelector('p#mainfooter').innerHTML = `${contador1} Participantes <input type="button" value="Conluido" id="proxeta" class="bota" onclick="mudarmain()">`
+        document.querySelector('p#mainfooter').innerHTML = `${contador1} Participantes >`
     }
     else{
         lista = document.querySelector('ul#namelist2')
         lista.innerHTML += `<li>${nome[contador1]}`
         contador1++
         nominp.value = ""
-        document.querySelector('p#mainfooter').innerHTML = `${contador1} Participantes <input type="button" value="Conluido" id="proxeta" class="bota" onclick="mudarmain()">`
+        document.querySelector('p#mainfooter').innerHTML = `${contador1} Participantes">`
 
     }
 }
