50cd685ad3a7c9ff90cefe48a7eea1ba54437f16
cbssoares <cbssdev@gmail.com>
2023-05-12 12:56:38 -0300

    seção de resultado criada

--- mecanismo.js
+++ mecanismo.js
@@ -1,6 +1,9 @@
 contador1 = 0
 document.querySelector('p#mainfooter').innerHTML = `${contador1} Participantes`
 nome = []
+selectOp = document.querySelector('#opcoes')
+mainResul =  document.querySelector('.main_resultado').classList
+mainPrinc = document.querySelector('.principal').classList
 document.querySelector('input#nomeinp').addEventListener("keyup", function(e) {
     if (e.code === 'Enter'){
         document.querySelector('input#botao').click()
@@ -12,8 +15,10 @@ function namesave(){
     if (nominp.value == ''){
         window.alert('Nome Não inserido')
     } else {
+        nome[contador1] = nominp.value
         lista = document.querySelector('ul#namelist1')
         lista.innerHTML += `<li>${nome[contador1]}`
+        selectOp.innerHTML += `<option> ${nome[contador1]} </option>`
         contador1++
         nominp.value = ""
         document.querySelector('p#mainfooter').innerHTML = `${contador1} Participantes `
@@ -23,5 +28,8 @@ function namesave(){
 }
 
 function mudarmain(){
-    window.alert('oi')
+    mainResul.remove('invisivel')
+    mainPrinc.remove('visivel')
+    mainResul.add('visivel') 
+    mainPrinc.add('invisivel')
 }
