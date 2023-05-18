0d9321ee314360c393fbca0cb38bb348c9aeca17
cbssoares <cbssdev@gmail.com>
2023-05-18 15:05:24 -0300

    sistema de remoção de nomes concluido

--- mecanismo.js
+++ mecanismo.js
@@ -2,6 +2,8 @@ contador1 = 0
 nome = []
 nome2 = []
 let amig = []
+limite = 0
+
 selectOp = document.querySelector('#opcoes')
 mainResul =  document.querySelector('.resultado').classList
 mainPrinc = document.querySelector('.main_inicio').classList
@@ -13,6 +15,7 @@ document.querySelector('input#nomeinp').addEventListener("keyup", function(e) {
     }
 })
 
+
 function namesave(){
     nominp = document.querySelector('input#nomeinp')
     if (nominp.value == ""){
@@ -22,13 +25,44 @@ function namesave(){
         nome2[contador1] = nominp.value
         loop = nome.length
         lista = document.querySelector('#namelist')
-        lista.innerHTML += `<li> ${nome[contador1]}`
+        lista.innerHTML += `<li> ${nome[contador1]}  <input type='button' value='x' class ="remover ${nome[contador1]}"> `
+        function listaMaker(){
+            const  itensInp = document.querySelectorAll('.remover')
+            function nomeNaLista(){
+                const nomeInp = itensInp[contador2].classList[1]
+                itensInp[contador2].addEventListener("click", function (){nomeRemover(nomeInp)})
+            }
+                
+            for (contador2 = 0; contador2 < loop; contador2++){
+                    nomeNaLista()
+            }
+        }
+       listaMaker()
+        
+        function nomeRemover(listaitem) {
+            tirarI = nome.indexOf(listaitem)
+            nome.splice(tirarI, 1)
+            nome2.splice(tirarI, 1)
+            loop = nome.length
+            lista.innerHTML = ''
+            for (contador2 = 0; contador2 < loop; contador2++){
+                lista.innerHTML += `<li> ${nome[contador2]}  <input type='button' value='x' class ="remover ${nome[contador2]}"> `
+               
+            }
+            listaMaker()
+            document.querySelector('#main_info_num_pessoas').innerHTML = `${loop} Participantes `
+            contador1--
+        }
+        
+
         contador1++
-        nominp.value = ""
-        document.querySelector('#main_info_num_pessoas').innerHTML = `${loop} Participantes `
-    }
     
+        document.querySelector('#main_info_num_pessoas').innerHTML = `${loop} Participantes `
+    }  
+    nominp.value = ""   
 }
+    
+
 
 
 
@@ -52,6 +86,7 @@ function telaInter() {
         selectOp.innerHTML = ' <option value="1" disabled selected hidden>Selecione o Nome do Participante</option>'
         loop2 = nome2.length
         nome.sort(() => Math.random() - 0.5)
+
         for(contador1 = 0; contador1 < loop; contador1++){
             contador2 = contador1 + 1
 
@@ -65,16 +100,27 @@ function telaInter() {
         for(contador1 = 0; contador1 < loop2; contador1++){
             selectOp.innerHTML += `<option value = '${nome2[contador1]}'> ${nome2[contador1]} </option>`
         }
+
         selectOp.value = '1'
         document.querySelector('.resultado_amigo').innerHTML = ''
-        let intervalo = setInterval(telaResultado, 3000)
-
+        let intervalo = setInterval(telaResultado, 2500);
+        let intervaloTimer = setInterval(timer, 400);
+        function timer(){
+            document.querySelector('.titulo-inter').innerHTML += '.';
+            limite++
+            if ( limite > 3) {
+                document.querySelector('.titulo-inter').innerHTML = 'Sorteando'
+                limite = 0
+            }
+        }
+        timer()
         function telaResultado() {
             mainInter.remove('visivel')
             mainResul.remove('invisivel')
             mainInter.add('invisivel') 
             mainResul.add('visivel')
             clearInterval(intervalo)
+            clearInterval(intervaloTimer)
         }
         
     } else{
